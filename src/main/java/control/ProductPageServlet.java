cef5d06903c5048dbd3e1eb49151c38b52ca9db6
d-quartucci <danielequartucci304@gmail.com>
2026-01-21 13:21:12 +0100

    Errore nel commit precedente: aggiunti adesso i file

---
+++ src/main/java/control/ProductPageServlet.java
@@ -0,0 +1,42 @@
+package control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Prodotto;
+import model.dao.ProdottoDAO;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+@WebServlet("/ProductPageServlet")
+public class ProductPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ProductPageServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DataSource ds = (DataSource) getServletContext().getAttribute("ds");
+		int id = Integer.parseInt(request.getParameter("prodottoId"));
+		ProdottoDAO pDAO =  new ProdottoDAO(ds);
+		try{
+			Prodotto pBean = pDAO.doRetrieveByKey(id);
+			request.setAttribute("prodotto", pBean);
+			request.getRequestDispatcher("/common/product.jsp").forward(request, response);
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+			response.sendRedirect(request.getContextPath() + "/common/error.jsp");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/common/product.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>${prodotto.nome}</title>
+</head>
+<body>
+${prodotto.nome}
+</body>
+</html>
