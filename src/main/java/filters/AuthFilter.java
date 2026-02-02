c3e022544150ff5041606cbb0ec478e18a53ba3b
d-quartucci <danielequartucci304@gmail.com>
2026-02-02 12:06:45 +0100

    Creato filtro di gestione accessi

---
+++ src/main/java/filters/AuthFilter.java
@@ -0,0 +1,52 @@
+package filters;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Utente;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AuthFilter extends HttpFilter implements Filter {
+
+	private static final long serialVersionUID = 1L;
+
+	public AuthFilter() {
+        super();
+    }
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		Utente u = null;
+		HttpSession sessione = httpRequest.getSession(false);
+		if(sessione != null) {
+			u = (Utente) sessione.getAttribute("utente");
+		}
+		
+		String path = httpRequest.getServletPath();
+		
+		//Controllo per le pagine user
+		if(path.contains("/user/") && u == null) {
+			httpResponse.sendRedirect(httpRequest.getContextPath() + "/common/login.jsp");
+			return;
+		} 
+		
+		//Controllo per le pagine admin
+		if(path.contains("/admin/") && (u == null || !"ADMIN".equals(u.getRuolo()))) {
+			httpResponse.sendRedirect(httpRequest.getContextPath() + "/common/index.jsp");
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+}
