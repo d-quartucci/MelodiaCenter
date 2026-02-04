0e7c8a238b7022e0f24d6b7bbd0deee95c255952
d-quartucci <danielequartucci304@gmail.com>
2026-02-04 11:43:11 +0100

    Modificato nome Servlet

---
+++ src/main/java/control/DoLoginServlet.java
@@ -0,0 +1,68 @@
+package control;
+
+import javax.sql.DataSource;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+
+import model.Utente;
+import model.dao.UtenteDAO;
+
+@WebServlet("/DoLoginServlet")
+public class DoLoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public DoLoginServlet() {
+        super();
+    }
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = toHash(request.getParameter("password"));
+		
+		DataSource ds = (DataSource) request.getServletContext().getAttribute("ds");
+		UtenteDAO uDAO = new UtenteDAO(ds);
+		try {
+			Utente u = uDAO.doRetrieveByLogin(email, password);
+			//Proviamo a fare il login
+			if(u != null) {
+				HttpSession sessione = request.getSession(); //Prendiamo la sessione per salvare le informazioni dell'utente
+				sessione.setAttribute("utente", u);
+				response.sendRedirect(request.getContextPath() + "/common/index.jsp");
+			}
+			else {
+				request.setAttribute("error", "Email o password non validi!");
+				request.getRequestDispatcher("/common/login.jsp").forward(request, response);
+			}
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+			request.getSession().setAttribute("errorMessage", "Errore durante il login: " + ex.getMessage());
+            response.sendRedirect(request.getContextPath() + "/common/error.jsp");
+		}
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		doPost(request, response);
+	}
+	
+	private String toHash(String password) {
+		String hashString = null;
+		try {
+			java.security.MessageDigest digest = java.security.MessageDigest.getInstance("SHA-512");
+			byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+			hashString = "";
+			for (int i = 0; i < hash.length; i++) {
+				hashString += Integer.toHexString((hash[i] & 0xFF) | 0x100).substring(1, 3);
+			}
+		} catch (java.security.NoSuchAlgorithmException e) {
+			System.out.println(e);
+		}
+		return hashString;
+	}
+}
---
+++ src/main/java/control/DoRegisterServlet.java
@@ -0,0 +1,62 @@
+package control;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import model.Utente;
+import model.dao.UtenteDAO;
+
+@WebServlet("/DoRegisterServlet")
+public class DoRegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public DoRegisterServlet() {
+        super();
+    }
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DataSource ds = (DataSource) request.getServletContext().getAttribute("ds");
+		
+		//Creo il nuovo utente con i parametri dati dal form
+		Utente u = new Utente();
+		u.setNome(request.getParameter("nome"));
+		u.setCognome(request.getParameter("cognome"));
+		u.setEmail(request.getParameter("email"));
+		u.setPassword(toHash(request.getParameter("password")));
+		u.setRuolo("USER");
+		u.setTelefono(request.getParameter("tel"));
+		
+		try {
+			UtenteDAO uDAO = new UtenteDAO(ds);
+			uDAO.doSaveOrUpdate(u); //Non verrà fatto mai l'update perché viene verificato per mezzo di JS che l'email non sia già stata utilizzata in precedenza 
+			response.sendRedirect(request.getContextPath() + "/common/login.jsp");
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+			request.getSession().setAttribute("errorMessage", "Errore durante la registrazione: " + ex.getMessage());
+            response.sendRedirect(request.getContextPath() + "/common/error.jsp");
+		}
+	}
+	
+	private String toHash(String password) {
+		String hashString = null;
+		try {
+			java.security.MessageDigest digest = java.security.MessageDigest.getInstance("SHA-512");
+			byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+			hashString = "";
+			for (int i = 0; i < hash.length; i++) {
+				hashString += Integer.toHexString((hash[i] & 0xFF) | 0x100).substring(1, 3);
+			}
+		} catch (java.security.NoSuchAlgorithmException ex) {
+			ex.printStackTrace();
+		}
+		return hashString;
+	}
+}
